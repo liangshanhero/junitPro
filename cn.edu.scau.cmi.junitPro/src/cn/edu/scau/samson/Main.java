f280f6938c9e79c5d3d3522d9d19c7768fa3ea2c
Liang Zaoqing <Liang Zaoqing@DESKTOP-L8402IO>
2018-02-27 19:24:59 +0800

    测试

---
+++ cn.edu.scau.cmi.junitPro/src/cn/edu/scau/samson/Main.java
@@ -0,0 +1,24 @@
+package cn.edu.scau.samson;
+
+import org.eclipse.jdt.junit.wizards.NewTestCaseWizardPageOne;
+import org.eclipse.jdt.junit.wizards.NewTestCaseWizardPageTwo;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		NewTestCaseWizardPageTwo fPage2= new NewTestCaseWizardPageTwo();
+//		NewTestCaseWizardPageOne fPage1= new NewTestCaseWizardPageOne(fPage2);
+//		Composite composite= new Composite(null, SWT.NONE);
+//		fPage1.createTextArea(composite, 4);
+		System.out.println("Hello samson!");
+		//NewTestCaseWizardPageOne n1 = new NewTestCaseWizardPageOne();
+
+	}
+
+}
